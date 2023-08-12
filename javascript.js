dce7464c84d3c86e1160df1736b5a6e2529e209f
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-08-11 20:52:24 -0400

    Added local storage

--- javascript.js
+++ javascript.js
@@ -1,17 +1,3 @@
-let myLibrary = [];
-
-const reading = 'Reading';
-const planToRead = 'Plan to Read';
-const completed = 'Completed';
-
-const bookFormOverlay = document.querySelector('.form-overlay-bg');
-const bookForm = document.querySelector('.book-form');
-const bookTitle = document.getElementById('title');
-const bookAuthor = document.getElementById('author');
-const bookPages = document.getElementById('pages');
-const bookStatus = document.getElementById('status');
-const bookshelf = document.querySelector('.bookshelf');
-
 class Book {
     constructor(title, author, pages, status) {
         this.title = title;
@@ -23,23 +9,31 @@ class Book {
 
 // Function that takes the user's input and stores the new book objects into an array (myLibrary)
 function addBookToLibrary() {
-    const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
-    myLibrary.push(newBook);
+    const bookTitle = document.getElementById('title');
+    const bookAuthor = document.getElementById('author');
+    const bookPages = document.getElementById('pages');
+    const bookStatus = document.getElementById('status');
+    const localLibrary = JSON.parse(localStorage.getItem("library"));
+    const newBook = new Book(bookTitle.value, bookAuthor.value, parseInt(bookPages.value), bookStatus.value);
+    localLibrary.push(newBook);
 
+    updateLocalLibrary(localLibrary);
     // After adding the book to the library, display the books
     displayBooks();
 
-    // Clear and close the form
-    clearForm();
     closeForm();
 }
 
 // Display Books
 function displayBooks() {
+    const bookshelf = document.querySelector('.bookshelf');
+    const reading = 'Reading';
+    const planToRead = 'Plan to Read';
+    const localLibrary = JSON.parse(localStorage.getItem("library"));
     // Calls the function to clear the bookshelf
     clearBookShelf();
 
-    for(let i = 0; i < myLibrary.length; i++) {
+    for(let i = 0; i < localLibrary.length; i++) {
         // Card
         const card = document.createElement('div');
         card.classList.add('card');
@@ -49,23 +43,23 @@ function displayBooks() {
         const cardAuthor = document.createElement('p');
         const cardPages = document.createElement('p');
 
-        cardTitle.textContent = myLibrary[i]['title'];
-        cardAuthor.textContent = myLibrary[i]['author'];
-        cardPages.textContent = `${myLibrary[i]['pages']} pages`;
+        cardTitle.textContent = localLibrary[i]['title'];
+        cardAuthor.textContent = localLibrary[i]['author'];
+        cardPages.textContent = `${localLibrary[i]['pages']} pages`;
 
         cardTitle.id = 'card-title';
 
         // Card Status Button
         const cardButtonDiv = document.createElement('div');
-        cardButtonDiv.classList.add('card-button-div');
+        cardButtonDiv.classList.add('card-btn-div');
         const cardStatusButton = document.createElement('button');
-        cardStatusButton.textContent = myLibrary[i]['status'];
+        cardStatusButton.textContent = localLibrary[i]['status'];
         cardStatusButton.type = 'button';
         cardStatusButton.onclick = changeReadingStatus;
 
-        if (myLibrary[i]['status'] === reading) {
+        if (localLibrary[i]['status'] === reading) {
             cardStatusButton.classList.add('reading');
-        } else if (myLibrary[i]['status'] === planToRead) {
+        } else if (localLibrary[i]['status'] === planToRead) {
             cardStatusButton.classList.add('planToRead');
         } else {
             cardStatusButton.classList.add('completed');
@@ -94,57 +88,108 @@ function displayBooks() {
 
 // On button click, change it to the next select option; re-display the bookshelf
 function changeReadingStatus() {
+    const reading = 'Reading';
+    const planToRead = 'Plan to Read';
+    const completed = 'Completed';
     // get the index of the card
     const cardIndex = parseInt(this.parentElement.parentElement.dataset.index);
+    const localLibrary = JSON.parse(localStorage.getItem("library"));
 
     // Reading -> Plan to Read -> Completed
     if (this.textContent === reading) {
-        myLibrary[cardIndex]['status'] = planToRead;
+        localLibrary[cardIndex]['status'] = planToRead;
     } else if (this.textContent === planToRead) {
-        myLibrary[cardIndex]['status'] = completed;
+        localLibrary[cardIndex]['status'] = completed;
     } else {
-        myLibrary[cardIndex]['status'] = reading;
+        localLibrary[cardIndex]['status'] = reading;
     }
 
+    updateLocalLibrary(localLibrary);
     displayBooks();
 }
 
 // On button click, remove the card from the array; re-display the bookshelf
 function removeCard() {
     const cardIndex = parseInt(this.parentElement.parentElement.dataset.index);
+    const localLibrary = JSON.parse(localStorage.getItem("library"));
 
-    myLibrary.splice(cardIndex, 1);
+    localLibrary.splice(cardIndex, 1);
+    updateLocalLibrary(localLibrary);
 
     displayBooks();
 }
 
 // Clear the bookshelf
 function clearBookShelf() {
+    const bookshelf = document.querySelector('.bookshelf');
     bookshelf.innerHTML = '';
 }
 
 // Form Functions (open, close, clear)
 function openForm() {
+    const bookFormOverlay = document.querySelector('.form-overlay-bg');
+    const bookForm = document.querySelector('.book-form');
     bookForm.style.display = 'block';
     bookFormOverlay.style.display = 'block';
 }
 
 function closeForm() {
+    const bookFormOverlay = document.querySelector('.form-overlay-bg');
+    const bookForm = document.querySelector('.book-form');
     bookForm.style.display = 'none';
     bookFormOverlay.style.display = 'none';
     clearForm();
 }
 
 function clearForm() {
+    const bookTitle = document.getElementById('title');
+    const bookAuthor = document.getElementById('author');
+    const bookPages = document.getElementById('pages');
+    const bookStatus = document.getElementById('status');
     bookTitle.value = '';
     bookAuthor.value = '';
     bookPages.value = '';
     bookStatus.value = '';
 }
 
-// TESTING
-const test1 = new Book('When Heaven and Earth Changed Places', 'Le Ly Hayslip, Jay Wurts', '464', completed);
-const test2 = new Book('Before the Coffee Gets Cold', 'Toshikazu Kawaguchi', '272', reading);
-const test3 = new Book('Yellow Face', 'R. F. Kuang', '336', planToRead);
-myLibrary.push(test1, test2, test3);
-displayBooks();
+// If there is a "library" item in localStorage, then display the local storage
+// Else, create a "library" with an empty array and load the library with sample books
+function runLocalStorage() {
+    if (localStorage.getItem("library")) { 
+        displayBooks();
+    } else { 
+        localStorage.setItem("library", JSON.stringify([]));
+        addSampleBooksToLibrary();
+    }
+}
+
+// Updates the "library" in localStorage with the updated library array
+function updateLocalLibrary(updatedLibrary) {
+    localStorage.setItem("library", JSON.stringify(updatedLibrary));
+}
+
+// Adds sample books to the "library" in localStorage and updates it
+function addSampleBooksToLibrary() {
+    const reading = 'Reading';
+    const planToRead = 'Plan to Read';
+    const completed = 'Completed';
+    const myLibrary = [];
+
+    const test1 = new Book('When Heaven and Earth Changed Places', 'Le Ly Hayslip, Jay Wurts', 464, completed);
+    const test2 = new Book('Before the Coffee Gets Cold', 'Toshikazu Kawaguchi', 272, reading);
+    const test3 = new Book('Yellow Face', 'R. F. Kuang', 336, planToRead);
+    myLibrary.push(test1, test2, test3);
+
+    updateLocalLibrary(myLibrary);
+    displayBooks();
+}
+
+// Clears the local storage
+function clearLocalStorage() {
+    if (confirm("Are you sure you want to clear the local storage? This action will restore the library to its original state and refresh the page.\nThis action cannot be undone.")) {
+        localStorage.clear();
+        location.reload();
+    }
+}
+
+runLocalStorage();
